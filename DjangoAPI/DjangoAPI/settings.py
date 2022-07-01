4d87fb21382421c1e9218025b1b5179bc27bb5c0
Piotr Barabasz <barabasz.piotrek@gmail.com>
2022-07-02 01:23:47 +0200

    Changing Django version & adding API

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from concurrent.futures.process import _threads_wakeups
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,9 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'EmployeeApp.apps.EmployeeappConfig',
+    'rest_framework'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
